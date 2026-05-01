ce33554fa1c8b3f7cb2c3b7149c48b9d0b994047
wanwusangzhigit <wanwusangzhigit@users.noreply.github.com>
2026-05-01 11:44:54 +0000

    feat: WinDarling: 在 Windows 上运行 macOS 应用
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/syscall.c
+++ src/syscall.c
@@ -7,11 +7,6 @@
     #include <Windows.h>
     #include <process.h>
     
-    #define pid_t int
-    #define uid_t int
-    #define gid_t int
-    #define mode_t int
-    
     #define SEEK_SET 0
     #define SEEK_CUR 1
     #define SEEK_END 2
