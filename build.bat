8d7b8146445ad4bf31f4400c4e687c2771d432c7
wanwusangzhigit <wanwusangzhigit@users.noreply.github.com>
2026-05-01 10:23:13 +0000

    fix: 更新 Windows 构建脚本，添加 Visual Studio 环境设置

--- build.bat
+++ build.bat
@@ -5,7 +5,15 @@ set "BIN_DIR=bin"
 
 if not exist "%BIN_DIR%" mkdir "%BIN_DIR%"
 
-cl /nologo /W3 /O2 /I"%SRC_DIR%" "%SRC_DIR%\macho.c" "%SRC_DIR%\syscall.c" "%SRC_DIR%\vfs.c" "%SRC_DIR%\dyld.c" "%SRC_DIR%\test_loader.c" /Fe"%BIN_DIR%\test_loader.exe"
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set "VS_PATH=%%i"
+)
+
+if exist "%VS_PATH%\Common7\Tools\VsDevCmd.bat" (
+  call "%VS_PATH%\Common7\Tools\VsDevCmd.bat"
+)
+
+cl /nologo /W3 /O2 /I"%SRC_DIR%" "%SRC_DIR%\macho.c" "%SRC_DIR%\syscall.c" "%SRC_DIR%\vfs.c" "%SRC_DIR%\dyld.c" "%SRC_DIR%\platform.c" "%SRC_DIR%\test_loader.c" /Fe"%BIN_DIR%\test_loader.exe"
 
 if %ERRORLEVEL% equ 0 (
     echo Build successful!
